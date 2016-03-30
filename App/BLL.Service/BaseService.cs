7166524b45102d2462b63d3801cacaaef43b5d4f
Jigkoh 0220 <Jigkoh 0220>
2016-03-30 10:03:07 +0700

    initail project template

---
+++ App/BLL.Service/BaseService.cs
@@ -0,0 +1,32 @@
+﻿//using BLL.Service.Interface;
+//using DAL.Entity.Models;
+using DAL.Repository;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.ModelBinding;
+
+namespace BLL.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BaseService([Dependency("UnitOfWork")] IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+
+
+    }
+
+}
+
---
+++ App/BLL.Service/Interface/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿//using DAL.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.ModelBinding;
+
+namespace BLL.Service
+{
+    public interface IBaseService : IDisposable
+    {
+     
+    }
+}
